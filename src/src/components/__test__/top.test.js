fe3a7b471c108c5465c8d768e091ae35cd8923cc
lauragonro <141372877+lauragonro@users.noreply.github.com>
2023-11-01 15:32:03 -0500

    Update top.test.js

--- src/src/components/__test__/top.test.js
+++ src/src/components/__test__/top.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Head from '../top'; // Importa el componente que deseas probar
+import Head from '../top';
+import GetHome from './home';
+import GetCart from './cart';
+import UserInfo from './getUser';
 
 test('Renderiza Head correctamente', () => {
-  render(Head); // Renderiza el componente
-
+  const { getByTestId } = render(<Head />);
+ 
+  const header = getByTestId('header');
+  expect(header).toBeInTheDocument();
+ 
+  const homeComponent = getByTestId('home-component');
+  expect(homeComponent).toBeInTheDocument();
+ 
+  const cartComponent = getByTestId('cart-component');
+  expect(cartComponent).toBeInTheDocument();
+ 
+  const userInfoComponent = getByTestId('user-info-component');
+  expect(userInfoComponent).toBeInTheDocument();
 
 });
