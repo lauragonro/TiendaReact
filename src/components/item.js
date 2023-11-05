3027989ec0d220e7c6a63037d4d7158c8782b22f
Juliana <141372877+lauragonro@users.noreply.github.com>
2023-11-05 03:17:04 +0000

    Update item.js

--- src/components/item.js
+++ src/components/item.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function ShowItem(props) {
   return (
     <div className="itemCard">
-      <img src={props.image}></img>
-      <Link to={`/itemDescription/${props.id}`} style={{ textDecoration: 'none' }}>
-      <h3 className="itemTitle">{props.title}</h3>
+      <img src={props.image} alt={props.category}></img>
+      <Link
+        to={`/itemDescription/${props.id}`}
+        style={{ textDecoration: "none" }}
+      >
+        <h3 className="itemTitle">{props.title}</h3>
       </Link>
       <h2 className="itemPrice">${props.price}</h2>
     </div>
-  )
-};
+  );
+}
 export default ShowItem;
